BayLibre/android_kernel_common

ANDROID: PM: sleep: export device_pm_callback_start/end trace event
Export the device_pm_callback_start/end tracepoint so it can be used
in loadable modules.

Bug: 158528747

Change-Id: I31b99032b351d6a548f6c0ee4e02655aefae647f
Signed-off-by: Changki Kim <changki.kim@samsung.com>

kernel/trace/power-traces.c
@@ -18,4 +18,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(suspend_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(cpu_idle);
 EXPORT_TRACEPOINT_SYMBOL_GPL(cpu_frequency);
 EXPORT_TRACEPOINT_SYMBOL_GPL(powernv_throttle);
-
+EXPORT_TRACEPOINT_SYMBOL_GPLT(device_pm_callback_start);
+EXPORT_TRACEPOINT_SYMBOL_GPLT(device_pm_callback_end);

FgnorDevelopment/android_kernel_samsung_universal8825

Change-Id: Icfe12f496f25d3b73dbd70394dc85d197709671a
(cherry picked from commit d16111a8cf537426853300149b2ce65790fb650c)
Signed-off-by: Jonglin Lee <jonglin@google.com>

kernel/trace/power-traces.c
@@ -18,4 +18,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(suspend_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(cpu_idle);
 EXPORT_TRACEPOINT_SYMBOL_GPL(cpu_frequency);
 EXPORT_TRACEPOINT_SYMBOL_GPL(powernv_throttle);
-
+EXPORT_TRACEPOINT_SYMBOL_GPL(device_pm_callback_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(device_pm_callback_end);