FgnorDevelopment/android_kernel_samsung_universal8825

commit 5505db8665c836e6b4bb3b3e329918bfb1e362ff

FROMGIT: usb: gadget: configfs: Add a specific configFS reset callback
In order for configFS based USB gadgets to set the proper charge current
for bus reset scenarios, expose a separate reset callback to set the
current to 100mA based on the USB battery charging specification.

Reviewed-by: Peter Chen <peter.chen@nxp.com>
Signed-off-by: Wesley Cheng <wcheng@codeaurora.org>
Link: https://lore.kernel.org/r/1609283011-21997-4-git-send-email-wcheng@codeaurora.org
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
(cherry picked from commit 4d7aae9f7a18f27ade0fc1d275f272f23529d6ba
 https://git.kernel.org/pub/scm/linux/kernel/git/gregkh/usb.git usb-next)
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
Change-Id: I57b8d8c5e6726c0922e91c2f2c83cb85aef0436c

drivers/usb/gadget/configfs.c
@@ -1647,6 +1647,28 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 
 #endif // CONFIG_USB_CONFIGFS_UEVENT
 
+static void configfs_composite_reset(struct usb_gadget *gadget)
+{
+	struct usb_composite_dev *cdev;
+	struct gadget_info *gi;
+	unsigned long flags;
+
+	cdev = get_gadget_data(gadget);
+	if (!cdev)
+		return;
+
+	gi = container_of(cdev, struct gadget_info, cdev);
+	spin_lock_irqsave(&gi->spinlock, flags);
+	cdev = get_gadget_data(gadget);
+	if (!cdev || gi->unbind) {
+		spin_unlock_irqrestore(&gi->spinlock, flags);
+		return;
+	}
+
+	composite_reset(gadget);
+	spin_unlock_irqrestore(&gi->spinlock, flags);
+}
+
 static void configfs_composite_suspend(struct usb_gadget *gadget)
 {
 	struct usb_composite_dev *cdev;
@@ -1701,7 +1723,7 @@ static const struct usb_gadget_driver configfs_driver_template = {
 	.disconnect     = android_disconnect,
 #else
 	.setup          = configfs_composite_setup,
-	.reset          = configfs_composite_disconnect,
+	.reset          = configfs_composite_reset,
 	.disconnect     = configfs_composite_disconnect,
 #endif
 	.suspend	= configfs_composite_suspend,

commit ee21b450a79ca6e427f701ae740bd14f3f9640bd

ANDROID: usb: gadget: configfs: Move CONFIGFS_UEVENT #endif
Move the CONFIG_USB_CONFIGFS_UEVENT #endif to cover the
configfs_composite_reset() function to prevent an unused function
warning when this config is unset.

Fixes: 5505db8665c8 ("FROMGIT: usb: gadget: configfs: Add a specific configFS reset callback")
Signed-off-by: J. Avila <elavila@google.com>
Change-Id: I61adc4516dc2574cc7d7a02bcc3fa7d86b8efd1d

drivers/usb/gadget/configfs.c
@@ -1645,8 +1645,6 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
 
-#endif // CONFIG_USB_CONFIGFS_UEVENT
-
 static void configfs_composite_reset(struct usb_gadget *gadget)
 {
 	struct usb_composite_dev *cdev;
@@ -1668,6 +1666,8 @@ static void configfs_composite_reset(struct usb_gadget *gadget)
 	composite_reset(gadget);
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
+
+#endif // CONFIG_USB_CONFIGFS_UEVENT
 
 static void configfs_composite_suspend(struct usb_gadget *gadget)
 {