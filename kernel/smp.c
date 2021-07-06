FgnorDevelopment/android_kernel_samsung_universal8825

commit 7f4d1ed1da1895baa0d19cc3ec95ba26323e6f0f

ANDROID: cpuidle: Update cpuidle_uninstall_idle_handler() to wakeup all online CPUs
wake_up_all_idle_cpus() will not wakeup paused CPUs since they are removed
from cpu_active_mask but paused CPUs can be in deep cpu idle and hence must
wakeup when uninstalling idle handler.

This change fixes this by introducing wake_up_all_online_idle_cpus() to
unconditionally wakeup all online idle CPUs and invoking same when uninstalling
cpu idle handler.

Bug: 192436062
Fixes: 632f50beac9e ("ANDROID: cpu/hotplug: add pause/resume_cpus interface")
Change-Id: I4afd4b7a17b87f9cc495e7009c9537888387f9ef
Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

kernel/smp.c
@@ -968,6 +968,27 @@ void wake_up_all_idle_cpus(void)
 EXPORT_SYMBOL_GPL(wake_up_all_idle_cpus);
 
 /**
+ * wake_up_all_online_idle_cpus - break all online cpus out of idle
+ * wake_up_all_online_idle_cpus try to break all online cpus which is in idle
+ * state even including idle polling cpus, for non-idle cpus, we will do nothing
+ * for them.
+ */
+void wake_up_all_online_idle_cpus(void)
+{
+	int cpu;
+
+	preempt_disable();
+	for_each_online_cpu(cpu) {
+		if (cpu == smp_processor_id())
+			continue;
+
+			wake_up_if_idle(cpu);
+	}
+	preempt_enable();
+}
+EXPORT_SYMBOL_GPL(wake_up_all_online_idle_cpus);
+
+/**
  * smp_call_on_cpu - Call a function on a specific cpu
  *
  * Used to call a function on a specific cpu and wait for it to return.

drivers/cpuidle/cpuidle.c
@@ -428,7 +428,7 @@ void cpuidle_uninstall_idle_handler(void)
 {
 	if (enabled_devices) {
 		initialized = 0;
-		wake_up_all_idle_cpus();
+		wake_up_all_online_idle_cpus();
 	}
 
 	/*

include/linux/smp.h
@@ -127,6 +127,7 @@ int smp_call_function_any(const struct cpumask *mask,
 
 void kick_all_cpus_sync(void);
 void wake_up_all_idle_cpus(void);
+void wake_up_all_online_idle_cpus(void);
 
 /*
  * Generic and arch helpers
@@ -182,6 +183,7 @@ smp_call_function_any(const struct cpumask *mask, smp_call_func_t func,
 
 static inline void kick_all_cpus_sync(void) {  }
 static inline void wake_up_all_idle_cpus(void) {  }
+static inline void wake_up_all_online_idle_cpus(void) {  }
 
 #ifdef CONFIG_UP_LATE_INIT
 extern void __init up_late_init(void);

commit 1537c9df0da9f62ec4ae1a65a833418a18a1eecb

ANDROID: Fix kernelci warnings for indentation in smp.c
Fix warnings reported by kernelci due to incorrect indentatio:

    kernel/smp.c:982:3: warning: this ‘if’ clause does not guard

Fixes: 7f4d1ed1da18 ("ANDROID: cpuidle: Update cpuidle_uninstall_idle_handler()
to wakeup all online CPUs")
Signed-off-by: Todd Kjos <tkjos@google.com>
Change-Id: Ide771342558de321154696f9fe1272750a773853

kernel/smp.c
@@ -982,7 +982,7 @@ void wake_up_all_online_idle_cpus(void)
 		if (cpu == smp_processor_id())
 			continue;
 
-			wake_up_if_idle(cpu);
+		wake_up_if_idle(cpu);
 	}
 	preempt_enable();
 }