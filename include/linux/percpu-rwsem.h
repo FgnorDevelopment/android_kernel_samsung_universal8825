BayLibre/android_kernel_common

ANDROID: fix ABI breakage caused by percpu_rw_semaphore changes
percpu_rw_semaphore changes to allow calling percpu_free_rwsem in atomic
context cause ABI breakage. Introduce percpu_free_rwsem_atomic wrapper
and change percpu_rwsem_destroy to use it in order to keep
percpu_rw_semaphore struct intact and fix ABI breakage.

Bug: 161210518
Signed-off-by: Suren Baghdasaryan <surenb@google.com>
Change-Id: I198a6381fb48059f2aaa2ec38b8c1e5e5e936bb0

include/linux/percpu-rwsem.h
@@ -12,22 +12,18 @@
 struct percpu_rw_semaphore {
 	struct rcu_sync		rss;
 	unsigned int __percpu	*read_count;
-	/*
-	 * dtor and dtor_list are used during object destruction when
-	 * writer and waiters can't be used, therefore reusing the same space.
-	 */
-	union {
-		struct rcuwait		writer;
-		void (*dtor)(struct percpu_rw_semaphore *sem);
-	};
-	union {
-		wait_queue_head_t	waiters;
-		struct list_head	dtor_list;
-	};
+	struct rcuwait		writer;
+	wait_queue_head_t	waiters;
 	atomic_t		block;
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map	dep_map;
 #endif
+};
+
+struct percpu_rw_semaphore_atomic {
+	struct percpu_rw_semaphore rw_sem;
+	void (*dtor)(struct percpu_rw_semaphore_atomic *sem);
+	struct list_head dtor_list;
 };
 
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
@@ -146,8 +142,8 @@ extern void percpu_free_rwsem(struct percpu_rw_semaphore *);
  * using kfree. If called in atomic context, the destruction is performed
  * asynchronously from a worker thread.
  */
-extern void percpu_rwsem_destroy(struct percpu_rw_semaphore *sem,
-				 void (*dtor)(struct percpu_rw_semaphore *));
+extern void percpu_rwsem_destroy(struct percpu_rw_semaphore_atomic *sem,
+				 void (*dtor)(struct percpu_rw_semaphore_atomic *));
 
 #define percpu_init_rwsem(sem)					\
 ({								\

FgnorDevelopment/android_kernel_samsung_universal8825

include/linux/percpu-rwsem.h
@@ -13,18 +13,16 @@ struct percpu_rw_semaphore {
 	struct rcu_sync		rss;
 	unsigned int __percpu	*read_count;
 	struct rcuwait		writer;
-	/*
-	 * destroy_list_entry is used during object destruction when waiters
-	 * can't be used, therefore reusing the same space.
-	 */
-	union {
-		wait_queue_head_t	waiters;
-		struct list_head	destroy_list_entry;
-	};
+	wait_queue_head_t	waiters;
 	atomic_t		block;
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map	dep_map;
 #endif
+};
+
+struct percpu_rw_semaphore_atomic {
+	struct percpu_rw_semaphore rw_sem;
+	struct list_head destroy_list_entry;
 };
 
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
@@ -138,7 +136,7 @@ extern int __percpu_init_rwsem(struct percpu_rw_semaphore *,
 extern void percpu_free_rwsem(struct percpu_rw_semaphore *);
 
 /* Invokes percpu_free_rwsem and frees the semaphore from a worker thread. */
-extern void percpu_rwsem_async_destroy(struct percpu_rw_semaphore *sem);
+extern void percpu_rwsem_async_destroy(struct percpu_rw_semaphore_atomic *sem);
 
 #define percpu_init_rwsem(sem)					\
 ({								\